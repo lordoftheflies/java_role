bb982e836f0cf3af2357daa32969f1776ea70b0a
lordoftheflies <laszlo.hegedus@cherubits.hu>
2020-02-17 05:58:18 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+
+
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
 setup(
+    setup_requires=['pbr'],
+    pbr=True,
     name="java_role",
     version="0.1",
     packages=find_packages(),
